c4490bd3aa8be5eccb1b6ba0e37962ff7b5bcc37
Sathish235 <selenium235@gmail.com>
2020-04-20 17:26:15 +0530

    GIT CONFLICT FROM MASTER

--- gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
+++ gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
@@ -7,30 +7,6 @@ public class gitPractice2020 {
 	@Test
 	public void gitPrac2020() {
 		System.out.println("ORIGINAL");
-		System.out.println("CODE FROM BRANCH");
-		System.out.println("CODE FROM GITBRANCH");
-		System.out.println("CODE FROM GITBRANCH");
-		System.out.println("CODE FROM GITBRANCH");
-		System.out.println("CODE FROM GITBRANCH");
-	}
-	
-	@Test
-	public void gitPrac2021() {
-		System.out.println("ORIGINAL FROM newdevBranch");
-		System.out.println("CODE FROM newdevBranch");
-		System.out.println("CODE FROM newdevBranch");
-		System.out.println("CODE FROM newdevBranch");
-		System.out.println("CODE FROM newdevBranch");
-		System.out.println("CODE FROM newdevBranch");
-	}
-	
-	@Test
-	public void gitPrac2022() {
-		System.out.println("PUSH FROM GIT BRANCH to newdevBranch");
-	}
-	
-	@Test
-	public void git2020() {
-		System.out.println("FINAL COMMIT FROM MASTER BRANCH");
+		System.out.println("CREATING MERGE CONFLICT FROM MASTER");
 	}
 }
