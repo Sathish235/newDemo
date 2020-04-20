d652b1aa0a7e96ba61405ab098407683102c069b
Sathish235 <selenium235@gmail.com>
2020-04-20 16:34:32 +0530

    equalize masters

--- gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
+++ gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
@@ -13,34 +13,4 @@ public class gitPractice2020 {
 		System.out.println("CODE FROM GITBRANCH");
 		System.out.println("CODE FROM GITBRANCH");
 	}
-	
-	@Test
-	public void gitPrac2021() {
-		System.out.println("ORIGINAL");
-		System.out.println("CODE FROM BRANCH1");
-		System.out.println("CODE FROM GITBRANCH1");
-		System.out.println("CODE FROM GITBRANCH1");
-		System.out.println("CODE FROM GITBRANCH1");
-		System.out.println("CODE FROM GITBRANCH1");
-	}
-	
-	@Test
-	public void gitPrac2022() {
-		System.out.println("DEVBRANCH1");
-		System.out.println("CODE FROM DEVBRANCH1");
-		System.out.println("CODE FROM DEVBRANCH1");
-		System.out.println("CODE FROM DEVBRANCH1");
-		System.out.println("CODE FROM DEVBRANCH1");
-		System.out.println("CODE FROM DEVBRANCH1");
-	}
-	
-	@Test
-	public void gitPrac2023() {
-		System.out.println("DEVBRANCH1MASTER");
-		System.out.println("CODE FROM DEVBRANCH1MASTER");
-		System.out.println("CODE FROM DEVBRANCH1MASTER");
-		System.out.println("CODE FROM DEVBRANCH1MASTER");
-		System.out.println("CODE FROM DEVBRANCH1MASTER");
-		System.out.println("CODE FROM DEVBRANCH1MASTER");
-	}
 }
