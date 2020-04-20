bcc7e8ba7041c0242536dc886c6ef8b2aa9db8ef
Sathish235 <selenium235@gmail.com>
2020-04-20 15:49:56 +0530

    American guy commited

--- gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
+++ gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
@@ -13,4 +13,14 @@ public class gitPractice2020 {
 		System.out.println("CODE FROM GITBRANCH");
 		System.out.println("CODE FROM GITBRANCH");
 	}
+	
+	@Test
+	public void gitPrac2021() {
+		System.out.println("ORIGINAL");
+		System.out.println("CODE FROM BRANCH1");
+		System.out.println("CODE FROM GITBRANCH1");
+		System.out.println("CODE FROM GITBRANCH1");
+		System.out.println("CODE FROM GITBRANCH1");
+		System.out.println("CODE FROM GITBRANCH1");
+	}
 }
