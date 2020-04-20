daf50650933e20743c2af0cf550f9b1a3385c51a
Sathish235 <selenium235@gmail.com>
2020-04-20 15:19:15 +0530

    ORIGINAL COMMIT

--- gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
+++ gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
@@ -7,8 +7,6 @@ public class gitPractice2020 {
 	@Test
 	public void gitPrac2020() {
 
-		System.out.println("This is from original master");
-		System.out.println("new code change from newDemo master branch");
-		System.out.println("NEW CODE CHANGE FROM GIT PRACTICE 2020");
+		System.out.println("ORIGINAL");
 	}
 }
