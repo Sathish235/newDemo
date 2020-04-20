c76b81c8fc85b989a33c5325c5587ec289567f2b
Sathish235 <selenium235@gmail.com>
2020-04-20 13:33:11 +0530

    This is from GITDemo_newDemo master

---
+++ gitDemoProject2020/src/test/java/gitDemoPack2020/gitPractice2020.java
@@ -0,0 +1,13 @@
+package gitDemoPack2020;
+
+import org.testng.annotations.Test;
+
+public class gitPractice2020 {
+	
+	@Test
+	public void gitPrac2020() {
+		
+		System.out.println("This is from original master");
+		
+	}
+}
